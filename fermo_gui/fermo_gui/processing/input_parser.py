0ff57bdb62a036f38862000abd1ffc085096311d
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-01-29 20:21:16 +0100

    Implemented update to 0.7.0 including frontend changes

--- fermo_gui/fermo_gui/processing/input_parser.py
+++ fermo_gui/fermo_gui/processing/input_parser.py
@@ -567,6 +567,8 @@ class InputParser(BaseModel):
                         self.params["PhenoQualAssgnParameters"]["algorithm"] = str(val)
                     case "PhenoQualAssgnParametersValue":
                         self.params["PhenoQualAssgnParameters"]["value"] = str(val)
+                    case "PhenoQualAssgnParametersTest":
+                        self.params["PhenoQualAssgnParameters"]["test"] = str(val)
                     case "PhenoQualAssgnParametersPThreshold":
                         self.params["PhenoQualAssgnParameters"]["p_val_cutoff"] = float(
                             val
